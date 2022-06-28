4ac31c1a11dc1113bc4181062da4105d6c194020
Jennifer Alvarez <jennifer.s.alvarez93@gmail.com>
2022-06-28 18:03:56 -0300

    agregamos mensajes por diferentes pantallas

---
+++ js/1holamundo.js
@@ -0,0 +1,14 @@
+//esto es un comentario de una unica linea
+
+/* esto es un 
+comentario de
+multiples lineas */
+
+//mostrar msj en la pantalla o documento 
+document.write('Hola comision 6i');
+
+//mostrar mensaje a consola
+console.log("prueba de consola");
+
+//mostrar msj en ventana emergente
+alert("desde una ventana emergente");
