57fee84a4be069593b6c32305dd612bd76f47242
Jennifer Alvarez <jennifer.s.alvarez93@gmail.com>
2022-07-05 18:35:04 -0300

    ejercicio de bucles con while

---
+++ js/3Bucles.js
@@ -0,0 +1,14 @@
+/* Bucles
+
+while(condicion logica){
+    todas las lineas de codigo de lo que quiero que repita  el programa
+    cambiar la condicion logica
+}
+
+*/
+let cantidad = 1;
+
+while(cantidad <= 20){
+    document.write("<br> preparar mate y pasar al siguiente compañero, mate cebado n "+ cantidad);
+    cantidad++ // cantidad = cantidad+1;
+}
---
+++ js/ejercicios.js/ultimosEjercicios.js
@@ -0,0 +1,5 @@
+let frase = prompt ("ingrese una frase")
+console.log(frase);
+console.log (frase.length)
+console.log (frase.charAt (0))
+console.log (frase.charAt (frase.length-1))
