aa328f1ef37b9d891b84e484eadab97f97bf4f1c
Jennifer Alvarez <jennifer.s.alvarez93@gmail.com>
2022-07-25 16:15:34 -0300

    agregamos ejemplo de bucle do-while

--- js/3Bucles.js
+++ js/3Bucles.js
@@ -7,9 +7,22 @@ while(condicion logica){
 
 */
 
-let cantidad = 1;
+// let cantidad = 1;
 
-while(cantidad <= 20){
-    document.write("<br> preparar mate y pasar al siguiente compañero, mate cebado n "+ cantidad);
-    cantidad++ // cantidad = cantidad+1;
-}
+// while(cantidad <= 20){
+//     document.write("<br> preparar mate y pasar al siguiente compañero, mate cebado n "+ cantidad);
+//     cantidad++ // cantidad = cantidad+1;
+// }
+
+/* Bucle Do-while hacer... mientras. Ejecuta el bucle al menos una vez
+do{
+todo el codigo que quiero ejecutar x cantidad de veces
+cambiar condicion logica
+} while(condicion logica)
+*/
+
+// let mate = 1;
+// do{
+//     document.write("<br> preparar mate y pasar al siguiente compañero, mate cebado n "+ mate);
+//     mate++
+// }while (mate <=20)
