550d058aa450072298a0e137c356382ac56bbf37
Jennifer Alvarez <jennifer.s.alvarez93@gmail.com>
2022-07-26 10:53:25 -0300

    se agrega ej de ejercicio 9 tp1 con for

--- js/ejercicios.js/ejercicio9.js
+++ js/ejercicios.js/ejercicio9.js
@@ -1,8 +1,9 @@
-let frase = prompt ("ingrese una frase")
+let frase = prompt('Ingrese una frase de hasta 10 caracteres');
 console.log(frase);
-console.log (frase.length)
-console.log (frase.charAt (0))
-console.log (frase.charAt (frase.length-1))
+console.log(frase.length)
+console.log(frase.charAt(0))
+console.log(frase.charAt(frase.length-1))
+console.log(frase.toLowerCase());
 
 //actualizar la variable frase
 frase = frase.toLowerCase();
