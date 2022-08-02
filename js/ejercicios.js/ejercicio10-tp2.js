4957457ad39fc21d326e56d45ef00c060a457c69
Jennifer Alvarez <jennifer.s.alvarez93@gmail.com>
2022-08-01 21:03:18 -0300

    se agrega ejercicio 10 del tp2

--- js/ejercicios.js/ejercicio10-tp2.js
+++ js/ejercicios.js/ejercicio10-tp2.js
@@ -2,18 +2,21 @@
 // dibujar una tabla 
 // cada celda debe tener un numero descendente de multiplicar filas por columnas
 
-let filas = parseInt(prompt("Ingrese una cantidad de filas"));
-let columnas = parseInt(prompt("Ingrese una cantidad de columnas"));
+const filas = parseInt(prompt("Ingrese una cantidad de filas"));
+const columnas = parseInt(prompt("Ingrese una cantidad de columnas"));
+let resultado = filas * columnas;
 
 document.write(`<table> <tbody>`);
 
 // se encarga de dibujar las filas
 for(let indiceFila = 0; indiceFila < filas; indiceFila++){
-    document.write(`
-        <tr>
-            <td>prueba</td>
-            <td>datos</td>
-        </tr>`);
+    document.write(`<tr>`)
+
+    for(let indiceColumna = 0; indiceColumna < columnas; indiceColumna++){
+     document.write(`<td>${resultado}</td>`);
+     resultado--;
+     }
+    document.write(`</tr>`);
 }
 
 document.write(`</tbody> </table>`);
